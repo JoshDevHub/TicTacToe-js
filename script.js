8311d0510d51d8755d8f89f9c4f684bfe623b53e
jmsmith1018 <jmsmith1018@gmail.com>
2022-12-06 14:35:36 -0700

    feat: fully complete end game and rematch functionality

--- script.js
+++ script.js
@@ -64,19 +64,17 @@ const GameBoard = () => {
 // Player Factory
 const Player = (symbol) => {
   const playerSymbol = symbol;
-
   const getSymbol = () => playerSymbol;
-
   return { getSymbol };
 }
 
 const TicTacToe = (pubSub) => {
-  const board = GameBoard();
+  let board = GameBoard();
   const player1 = Player("X");
   const player2 = Player("O");
   let currentPlayer = player1;
 
-  const getCurrentPlayer = () => currentPlayer.getSymbol();
+  const getCurrentSymbol = () => currentPlayer.getSymbol();
 
   const getBoardData = () => board.getData();
 
@@ -86,18 +84,11 @@ const TicTacToe = (pubSub) => {
 
   const makeMove = (position) => {
     if (board.isPositionAvailable(position)) {
-      const currentSymbol = currentPlayer.getSymbol();
-      board.placeSymbol(currentSymbol, position);
+      board.placeSymbol(getCurrentSymbol(), position);
+      pubSub.publish('boardUpdated', board.getData());
       if (isGameOver()) return;
 
       changeTurns();
-      pubSub.publish('moveExecuted', board.getData());
-    }
-  }
-
-  const isWinner = () => {
-    if (board.hasThreeInRow()) {
-      pubSub.publish('winEvent', getCurrentPlayer());
     }
   }
 
@@ -105,14 +96,29 @@ const TicTacToe = (pubSub) => {
   const isDraw = () => board.isFull();
   const isGameOver = () => isWinner() || isDraw();
 
+  const checkGameOver = () => {
+    if (isWinner()) {
+      pubSub.publish('gameOver', getCurrentSymbol());
+    } else if (isDraw()) {
+      pubSub.publish('gameOver', null);
+    }
+  }
+
+  const resetGame = () => {
+    board = GameBoard();
+    currentPlayer = player1;
+    pubSub.publish('boardUpdated', board.getData());
+  }
+
   pubSub.subscribe('makeMove', makeMove);
-  return { getCurrentPlayer, getBoardData, isDraw };
+  pubSub.subscribe('makeMove', checkGameOver);
+  pubSub.subscribe('newGame', resetGame);
+  return { getBoardData };
 }
 
 const Display = (pubSub) => {
   const grid = document.getElementById("grid");
   const resultsContainer = document.getElementById("results")
-  const { move, getData, isWinner, getCurrentPlayer } = actions;
 
   const squareClickHandler = (event) => {
     const row = event.target.parentNode.getAttribute("data-row")
@@ -124,10 +130,25 @@ const Display = (pubSub) => {
 
   grid.addEventListener("click", squareClickHandler);
 
-  const renderGameOver = () => {
-    grid.removeEventListener("click", squareClickHandler);
-    resultsContainer.textContent = `${getCurrentPlayer()} has won the game!`;
+  const renderGameOver = (winningPlayer) => {
+    let resultText;
+    if (winningPlayer) {
+      resultText = `${winningPlayer} has won the game!`;
+    } else {
+      resultText = "Tie game!";
+    }
+
+    resultsContainer.textContent = resultText;
+    toggleModal();
   };
+
+  const addPlayAgainListener = () => {
+    const replayButton = document.querySelector("button");
+    replayButton.addEventListener("click", () => {
+      pubSub.publish("newGame");
+      toggleModal();
+    })
+  }
 
   const renderGrid = (modelData) => {
     grid.replaceChildren();
@@ -143,14 +164,22 @@ const Display = (pubSub) => {
 
     rowData.forEach((position, index) => {
       const square = row.querySelector(`[data-col='${index}']`);
-      console.log(square === "X");
       square.textContent = position;
     });
     row.setAttribute("data-row", rowNum);
     grid.appendChild(clone);
   }
 
-  return { renderGrid }
+  const toggleModal = () => {
+    document
+      .querySelectorAll("[data-toggle='closed']")
+      .forEach((node) => node.classList.toggle('closed'));
+  }
+
+  pubSub.subscribe('boardUpdated', renderGrid);
+  pubSub.subscribe('gameOver', renderGameOver);
+
+  return { renderGrid, addPlayAgainListener }
 }
 
 const controller = (() => {
@@ -163,9 +192,7 @@ const controller = (() => {
     }
 
     const publish = (eventName, data) => {
-      if (events[eventName]) {
-        events[eventName].forEach((fn) => fn(data));
-      }
+      events[eventName]?.forEach((fn) => fn(data));
     }
 
     return { subscribe, publish };
@@ -175,4 +202,5 @@ const controller = (() => {
   const view = Display(pubSub);
 
   view.renderGrid(model.getBoardData());
+  view.addPlayAgainListener();
 })();
